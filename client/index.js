6b30a4afb9a2cbe5a5de080d0e43cf63ccfd70ee
npaul008 <npaulemon@gmail.com>
2021-05-29 14:22:15 -0400

    added code to capture audio data

--- client/index.js
+++ client/index.js
@@ -1,3 +1,6 @@
+STREAMING = false;
+streamObject = null;
+
 const getAudioDevices = function () {
     return new Promise(function(resolve,reject) {
         navigator.mediaDevices.enumerateDevices()
@@ -20,9 +23,89 @@ async function populateAudioDeviceSelect () {
     });
 }
 
+const displaySoundLevels = function () {
+    let array = new Uint8Array(streamObject.controller.analyzer.frequencyBinCount);
+    streamObject.controller.analyzer.getByteFrequencyData(array);
+
+    let values = 0;
+    for(let i = 0; i < array.length; i++) {
+        values += array[i];
+    }
+
+    let avg = values / array.length;
+    document.getElementById('audioLevel').value = Math.round(avg);
+}
+
+const handleAudioData = function (event) {
+    // console.log(event.inputBuffer.getChannelData(0));
+    displaySoundLevels();
+}
+
+const startStream = function () {
+    STREAMING = true;
+
+    navigator.mediaDevices.getUserMedia({
+        audio:{
+            deviceId:document.getElementById('audioSource').querySelector(':checked').value
+        }
+    })
+    .then(stream => {
+        console.log(stream);
+
+        let audioContext = new AudioContext();
+        let analyzer = audioContext.createAnalyser();
+        let microphone = audioContext.createMediaStreamSource(stream);
+        let javascriptNode = audioContext.createScriptProcessor(2048,1,1);
+
+        microphone.connect(analyzer);
+        analyzer.connect(javascriptNode);
+        javascriptNode.connect(audioContext.destination);
+
+        javascriptNode.addEventListener('audioprocess',handleAudioData);
+
+        streamObject = stream;
+
+        streamObject.controller = {
+            audioContext,
+            microphone,
+            analyzer,
+            javascriptNode
+        }
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+const stopStream = function () {
+    STREAMING = false;
+
+    if( streamObject !== null ) {
+        streamObject.controller.audioContext.close();
+        streamObject.controller.analyzer.disconnect();
+        streamObject.controller.microphone.disconnect();
+        streamObject.controller.javascriptNode.disconnect();
+        streamObject.controller.javascriptNode.removeEventListener('audioprocess',handleAudioData);
+
+        streamObject = null;
+    }
+}
+
+const initEventListeners = function() {
+    document.querySelector('button[name="goLive"]').addEventListener("click",function(event){
+        if( STREAMING ) {
+            stopStream();
+        }
+        else {
+            startStream();
+        }
+    });
+}
+
 const onLoad = function () {
+    initEventListeners();
     populateAudioDeviceSelect();
-    console.log('hello world')
 }
 
 window.addEventListener("load",onLoad);
